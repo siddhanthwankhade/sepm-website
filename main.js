e55916bcfa4d06c54f41465783ade9e27cb44bb0
siddhanth <sidspeedup@gmail.com>
2024-04-13 03:48:35 +0530

    page 1 page 2 layout

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="html/page1.html" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <div class="card">
+    <a href="html/page1.html">
     <button class="button-85"> Publish your ad here </button>
+    </a>
     </div>
     <h1>......</h1>
     <h1>Empowering local businesses with dynamic digital advertising</h1>
